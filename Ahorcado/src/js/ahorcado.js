2da93003baa5dc6409071b73caaabfa17c51b312
Naty <nataliatur986@gmail.com>
2023-04-10 18:02:03 -0300

    10

--- Ahorcado/src/js/ahorcado.js
+++ Ahorcado/src/js/ahorcado.js
@@ -1,107 +1,101 @@
 
-    //variables y constantes globales
-    const palabrAdivinar = ingresarPalabra();
+    /* Grupo
+    Tur, Natalia
+    Amaya, Natalia
+     */
+    //variables globlales / si hago F5 se recarga la pagina nuevamente con estos valores
+    
+    
     let arrCoincidencias = []; 
+    const palabrAdivinar = ingresarPalabra();
     let intentos = 6;
-
     const btn = document.getElementById('jugar');
     btn.disabled =true;
     const casilla = document.getElementById('entrada');
     casilla.disabled =false;
-
-    /*El Document método querySelector() devuelve el primer Element dentro del documento
-    que coincide con el selector o grupo de 
-    selectores especificado. Si no se encuentran coincidencias, devuelve null.*/
-    const letra = document.querySelector('input');
-
-    //oninput es un evento que se dispara con cualquier entrada por pantalla
-    letra.oninput = function(){
-        soloLetras(letra.value, palabrAdivinar, arrCoincidencias);
-    };
-
-
     
 
+    //va a funcionar mientras se modifique el input
+    const letra = document.querySelector('input');
+
+    //cada vez que el usuario escriba algo en el imput ejecutar la siguiente funcion
+    letra.oninput = function(){
+        soloLetras(letra.value, palabrAdivinar,arrCoincidencias);
+    };
+
     function ingresarPalabra(){
-
-
+        //variables locales
+        /*El método prompt() muestra un diálogo con mensaje opcional, que solicita al 
+        usuario que introduzca un texto. */
         const palabra = prompt("Ingresa una palabra para adivinar!");
+        //split toma una cadena y la divide elemento a elemento
+        let palabramin =palabra.toLowerCase(); //convierto la palabra en minuscula -
        
-       /*El método split() divide un objeto de tipo String en un array 
-        de cadenas.*/
-        const arrPalabra = palabra.split("");
-        
+        const arrPalabra = palabramin.split("");
+        //Muestra un mensaje en la consola web (o del intérprete JavaScript).
+        //solo se ve si voy a la consola con F12
         console.log(arrPalabra);
-        
-        /*El getElementById()método de la Document interfaz 
-        devuelve un Element objeto que representa el elemento 
-        cuya id coincide con la cadena especificada.
-
-        La propiedad Element.innerHTML devuelve 
-        o establece la sintaxis HTML*/
+        /*La propiedad Element.innerHTML devuelve o establece la sintaxis HTML describiendo 
+        los descendientes del elemento.*/
+        //construye la tabla donde esta el id "tablero", usa comillas invertidas
         document.getElementById("tablero").innerHTML = `
-            <table border="1">
+            <table > 
                 <tr>
                     ${creaTablero(arrPalabra)}    
                 </tr>    
             </table>
         `;
+        //En el codigo html si quiero ir a javascript uso el signo ${}
         return arrPalabra;
     };
 
-    
 
     function creaTablero(arrPalabra){
+
         let tablero = "";
 
-        /*El método forEach()ejecuta 
-        la función indicada una vez por cada elemento del arreglo.*/
-        //Función flecha letra(argumento); lo siguiente es el return
-        
+        //forEach es como el for pero itera sobre un array
+        /*dentro del forEach hay una funcion flecha pequeña, letra va a tomar
+        el valor de cada iteracion*/
         arrPalabra.forEach(letra => {
-        
             tablero = tablero + "<td> ? </td>";
         });
         return tablero;
     }
 
     function soloLetras(cadena, palabrAdivinar,arrCoincidencias){
-
-        //El objeto RegExp se utiliza para hacer que solo sean letras.
+        //pattern restringue que solo ingrese solo letras
         const pattern = new RegExp('[a-zA-Z]');
-        
-        
-        console.log(pattern.test(cadena));
-        
-        /*El test()método:
-        Devuelve un valor booleano que indica si existe o no un patrón en una cadena buscada.*/
-        if(!pattern.test(cadena)){
-
+        let cadenamin =cadena.toLowerCase();
+        //verifica que lo que obtuve  coincide con el patron, en este caso letras
+        console.log(pattern.test(cadenamin));
+        /*si es falso, va a agarrar el imput y lo va a borrar*/
+        if(!pattern.test(cadenamin)){
             document.querySelector('input').value = "";
+            //me va a al id "status"
             document.getElementById("status").innerHTML = "Solo puedes ingresar letras!!!..";
             return false;
-
-        }else{
-
+            //si es verdadero
+        }else{ /*vuelve a crear una tabla con el resultado de la funcion 
+            buscarcoinCidencias*/
             document.getElementById("tablero").innerHTML = `
-            <table border="1">
+            <table  >
                 <tr>
-                    ${buscarCoincidencia(cadena,palabrAdivinar,arrCoincidencias)}    
+                    ${buscarCoincidencia(cadenamin,palabrAdivinar,arrCoincidencias)}    
                 </tr>    
             </table>
         `;
-
-        //limpia el cuadro de texto para ingresar otra letra
+            //limpia el cuadro de texto para ingresar otra letra
             document.querySelector('input').value = "";
             return true;
         }
     }
-
+    //si la variable esta en la palabra, la muestra y si no le coloca ?
     function buscarCoincidencia(letra, arrPalabra,arrCoincidencias){
         let tablero = "";
         let coincidencias = 0;
         let exito = true;
-
+        
         
         arrPalabra.forEach(caracter => {
             
@@ -110,32 +104,32 @@
 
             //if: filtra si el caracter ya fue identificado
             if(arrCoincidencias.includes(caracter)){
-                tablero = tablero + "<td>"+ caracter +" </td>";
+                tablero = tablero + "<td style='background-color:lightgreen'>"+ caracter +" </td>";
+                
 
             /*else if: si no fue identificado guarda el caracter en arrCoincidencias e incrementa coincidencias 
             para registrarlas y luego informar en la función leyendaCoincidencia*/ 
             } else if(caracter == letra){              
-                tablero = tablero + "<td>"+ caracter +" </td>"; 
+                tablero = tablero + "<td style='background-color:lightgreen'>"+ caracter +" </td>"; 
                 arrCoincidencias.push(caracter);//push inserta al final del array cada caracter
                 coincidencias = coincidencias + 1;
+                
+                
             
             //else: no hubo coincidencia, pongo la variable exito en false para luego evaluar los intentos
             } else{
                 tablero = tablero + "<td> ? </td>";
                 exito = false;
+                
             }
-            
-            
             
         });
 
         leyendaCoincidencia(coincidencias, letra);
 
         //si no hubo aciertos va perdiendo intentos
-        if(coincidencias==0)
-        {intentos--;
-
-            //segun el numero de intento cambio la direccion de la imagen
+        if(coincidencias==0){ 
+            intentos--; //segun el numero de intento cambio la direccion de la imagen
             if(intentos==5){document.getElementById('imagen').src = "img/ahorcado5.png"; }
             if(intentos==4){document.getElementById('imagen').src = "img/ahorcado4.png"; }
             if(intentos==3){document.getElementById('imagen').src = "img/ahorcado3.png"; }
@@ -144,22 +138,27 @@
             if(intentos==0){document.getElementById('imagen').src = "img/ahorcado0.png"; 
             casilla.disabled =true; //deshabilito el input
             btn.disabled =false;} //habilito el boton de volver a jugar
-        
+                
         }
+
+        
         console.log(arrCoincidencias + " " + intentos);//imprimo la letra que coincide e intentos que quedan
 
-        //
+
+
         if(exito){ leyendaFelicitaciones(true);}
         else if(intentos==0) {leyendaFelicitaciones(false);}
             
-               
+
         return tablero;
     }
 
     function leyendaCoincidencia(coincidencias, caracter){
         if(coincidencias > 0){
+            document.getElementById('status').style.backgroundColor="blue";//coidencia fondo azul
             document.getElementById("status").innerHTML = `Hubo ${coincidencias} coincidencias con la letra ` + caracter;
         }else{
+            document.getElementById('status').style.backgroundColor="red";//no coincidencia fondo rojo
             document.getElementById("status").innerHTML = `No hubo coinciencias nuevas con la letra ` + caracter + `, te quedan ` + (intentos-1) + ` intentos` ;
         }
     }
@@ -182,6 +181,7 @@
         
     }
 
+    
     
     
     
